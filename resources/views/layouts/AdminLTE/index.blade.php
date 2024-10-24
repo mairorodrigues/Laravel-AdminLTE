3fab935eae1c41a78393326a7f227a942e32abbb
Mairo Rodrigues <mairo.rodrigues@hotmail.com>
2024-10-24 19:04:25 -0300

    fix/ui-screen-layout

--- resources/views/layouts/AdminLTE/index.blade.php
+++ resources/views/layouts/AdminLTE/index.blade.php
@@ -18,7 +18,7 @@
             @include('layouts.AdminLTE._includes._menu_lateral')
             
             <div class="content-wrapper">
-                <nav class="navbar navbar-static-top" id="menu_sup_corpo" style="background-color:#d2d6de; margin-bottom:0; padding-bottom:0;navbar-header.a:color:#fff;">      
+                <nav class="navbar" id="menu_sup_corpo" style="background-color:#d2d6de; margin-bottom:0; padding-bottom:0;navbar-header.a:color:#fff;">      
                     <div class="navbar-header">
                         <a href="" class="navbar-brand" id="" style="color:#222d32;'"><i class="fa fa-@yield('icon_page')"></i> @yield('title')</a>
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-2" aria-expanded="false">
